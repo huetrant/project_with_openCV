ce544b8e5ee42a6f57507e7da84144fbb1ce2c9b
Hue Tran <126448548+huetrant@users.noreply.github.com>
2024-10-06 20:40:26 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,5 @@ import streamlit as st
 
 st.set_page_config(page_title="My Application", page_icon=":smiley:")
 
-# Tạo sidebar menu
-st.sidebar.title("Menu")
-st.sidebar.success("Select a demo above.")
+
 
